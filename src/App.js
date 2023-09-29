ef1f11f81a6e22fbec66e3475136bf2903d9906a
Rugved Gawas <Rugved Gawas@LAPTOP-O3QGH3B4.local>
2023-09-29 10:39:19 +0530

    boilerplate react setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Details from './Details.js'
+function App() {
+  return (
+    <>
+   <Details />
+  </>
+  );
+}
+
+export default App;
---
+++ src/Details.js
@@ -0,0 +1,19 @@
+import React, {useState}from 'react'
+
+function Details() {
+
+  return (
+    <div>
+      <form>
+        <div>
+            <label>CARDHOLDER NAME</label><br/>
+            <input
+           
+            />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Details
